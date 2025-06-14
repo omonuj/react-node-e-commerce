0ed05b628b75de6d94d6c3f7be5dff52b9f3868a
omonuj <jonahodoh@gmail.com>
2025-06-14 22:46:49 +0100

    Scaffold Express app with JSON body parsing

---
+++ src/ecommerce/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+require('dotenv').config();
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require('./routes/order');
+// api docs
