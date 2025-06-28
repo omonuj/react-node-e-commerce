ac2bb9795f01f841ab9a2e2d0756c389d15c4d9b
omonuj <jonahodoh@gmail.com>
2025-06-28 23:20:43 +0100

    Add photo buffer, category ref and timestamps to Product

--- src/ecommerce/models/product.js
+++ src/ecommerce/models/product.js
@@ -21,3 +21,27 @@ const productSchema = new mongoose.Schema(
             maxlength: 32
         },
         category: {
+            type: ObjectId,
+            ref: "Category",
+            required: true
+        },
+        quantity: {
+            type: Number
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        },
+        shipping: {
+            required: false,
+            type: Boolean
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
