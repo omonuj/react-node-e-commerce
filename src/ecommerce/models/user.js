b06c8292eb0a2fc9add056a35a9185a6e9a3285b
omonuj <jonahodoh@gmail.com>
2025-07-05 15:48:50 +0100

    Add authenticate and encryptPassword methods

--- src/ecommerce/models/user.js
+++ src/ecommerce/models/user.js
@@ -44,3 +44,27 @@ userSchema
         this._password = password;
         this.salt = uuidv1();
         this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    }
+};
+
+module.exports = mongoose.model('User', userSchema);
