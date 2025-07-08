cec558b14236ba55317008ebc8aa4c999a841db1
omonuj <jonahodoh@gmail.com>
2025-07-08 17:22:41 +0100

    Define Order schema referencing user and products

--- src/ecommerce/models/order.js
+++ src/ecommerce/models/order.js
@@ -16,3 +16,21 @@ const CartItem = mongoose.model("CartItem", CartItemSchema);
 
 const OrderSchema = new mongoose.Schema(
   {
+    products: [CartItemSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    status: {
+      type: String,
+      default: "Not processed",
+      enum: ["Not processed", "Processing", "Shipped", "Delivered", "Cancelled"] // enum means string objects
+    },
+    updated: Date,
+    user: { type: ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = { Order, CartItem };
