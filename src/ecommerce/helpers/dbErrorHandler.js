97dc9f3db824bf8ee0815703ad4e6a295360f5e9
omonuj <jonahodoh@gmail.com>
2025-07-13 13:49:37 +0100

    Handle cast and validation errors uniformly

--- src/ecommerce/helpers/dbErrorHandler.js
+++ src/ecommerce/helpers/dbErrorHandler.js
@@ -22,3 +22,27 @@ const uniqueMessage = error => {
 };
 
 /**
+ * Get the erroror message from error object
+ */
+exports.errorHandler = error => {
+    let message = "";
+
+    if (error.code) {
+        switch (error.code) {
+            case 11000:
+            case 11001:
+                message = uniqueMessage(error);
+                break;
+            default:
+                message = "Something went wrong";
+        }
+    } else {
+        for (let errorName in error.errorors) {
+            if (error.errorors[errorName].message)
+                message = error.errorors[errorName].message;
+        }
+    }
+
+    return message;
+};
+
