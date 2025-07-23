eb351cb6966bbb0e6eca03039b9b91bae05a7122
omonuj <jonahodoh@gmail.com>
2025-07-23 23:58:38 +0100

    Add signout handler

--- src/ecommerce/controllers/auth.js
+++ src/ecommerce/controllers/auth.js
@@ -50,3 +50,29 @@ exports.signin = (req, res) => {
             return res.status(400).json({
                 error: 'User with that email does not exist. Please signup'
             });
+        }
+        // if user is found make sure the email and password match
+        // create authenticate method in user model
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            });
+        }
+        // generate a signed token with user id and secret
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        // persist the token as 't' in cookie with expiry date
+        res.cookie('t', token, { expire: new Date() + 9999 });
+        // return response with user and token to frontend client
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, email, name, role } });
+    });
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie('t');
+    res.json({ message: 'Signout success' });
+};
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256'],
