aedc1316f7dcfb1030548c847846ef4ebd0916ee
omonuj <jonahodoh@gmail.com>
2025-07-24 19:11:45 +0100

    Add requireSignin and isAuth guards

--- src/ecommerce/controllers/auth.js
+++ src/ecommerce/controllers/auth.js
@@ -76,3 +76,29 @@ exports.signout = (req, res) => {
 exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
     algorithms: ['HS256'],
+    requestProperty: 'auth'
+});
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
+        return res.status(403).json({
+            error: 'Access denied'
+        });
+    }
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'Admin resourse! Access denied'
+        });
+    }
+    next();
+};
+
+/**
+ * google login full
+ * https://www.udemy.com/instructor/communication/qa/7520556/detail/
+ */
