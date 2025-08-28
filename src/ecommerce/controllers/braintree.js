e427fa9ba27eee1559c06fd15d28b4830d84163d
omonuj <jonahodoh@gmail.com>
2025-08-28 11:01:59 +0100

    Process Braintree checkout transaction

--- src/ecommerce/controllers/braintree.js
+++ src/ecommerce/controllers/braintree.js
@@ -29,3 +29,35 @@ exports.generateToken = (req, res) => {
     }
     gateway.clientToken.generate({}, function(err, response) {
         if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(response);
+        }
+    });
+};
+
+exports.processPayment = (req, res) => {
+    const gateway = getGateway();
+    if (!gateway) {
+        return res.status(503).json({ error: 'Payments are not configured on this server' });
+    }
+    let nonceFromTheClient = req.body.paymentMethodNonce;
+    let amountFromTheClient = req.body.amount;
+    // charge
+    let newTransaction = gateway.transaction.sale(
+        {
+            amount: amountFromTheClient,
+            paymentMethodNonce: nonceFromTheClient,
+            options: {
+                submitForSettlement: true
+            }
+        },
+        (error, result) => {
+            if (error) {
+                res.status(500).json(error);
+            } else {
+                res.json(result);
+            }
+        }
+    );
+};
