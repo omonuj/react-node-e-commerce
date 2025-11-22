3dd5fe2427d3fd5216ddd684d8265d63e17ae7a1
omonuj <jonahodoh@gmail.com>
2025-11-22 11:08:47 +0100

    Add itemTotal and cart count helpers

--- src/ecommerce-fnt/src/core/cartHelpers.js
+++ src/ecommerce-fnt/src/core/cartHelpers.js
@@ -42,3 +42,47 @@ export const getCart = () => {
             return JSON.parse(localStorage.getItem('cart'));
         }
     }
+    return [];
+};
+
+export const updateItem = (productId, count) => {
+    let cart = [];
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'));
+        }
+
+        cart.map((product, i) => {
+            if (product._id === productId) {
+                cart[i].count = count;
+            }
+        });
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+};
+
+export const removeItem = productId => {
+    let cart = [];
+    if (typeof window !== 'undefined') {
+        if (localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'));
+        }
+
+        cart.map((product, i) => {
+            if (product._id === productId) {
+                cart.splice(i, 1);
+            }
+        });
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+    return cart;
+};
+
+export const emptyCart = next => {
+    if (typeof window !== 'undefined') {
+        localStorage.removeItem('cart');
+        next();
+    }
+};
