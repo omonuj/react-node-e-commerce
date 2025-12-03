e932e7e8b16caef461e67c2bcf67fc5e3baf2264
omonuj <jonahodoh@gmail.com>
2025-12-03 10:10:06 +0100

    Add product Card with add-to-cart

---
+++ src/ecommerce-fnt/src/core/Card.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import ShowImage from './ShowImage';
+import moment from 'moment';
+import { addItem, updateItem, removeItem } from './cartHelpers';
+
+const Card = ({
+  product,
+  showViewProductButton = true,
+  showAddToCartButton = true,
+  cartUpdate = false,
+  showRemoveProductButton = false,
+  setRun = f => f,
+  run = undefined
+  // changeCartSize
+}) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+
+  const showViewButton = showViewProductButton => {
+    return (
+      showViewProductButton && (
+        <Link to={`/product/${product._id}`} className="mr-2">
+          <button className="btn btn-outline-primary mt-2 mb-2 card-btn-1">View Product</button>
+        </Link>
+      )
+    );
+  };
+  const addToCart = () => {
+    // console.log('added');
+    addItem(product, setRedirect(true));
+  };
+
+  const shouldRedirect = redirect => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
+  const showAddToCartBtn = showAddToCartButton => {
