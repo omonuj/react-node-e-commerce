bbfb760155f40d95de0ec871b55b612b184825a9
omonuj <jonahodoh@gmail.com>
2025-12-18 14:12:05 +0100

    Render searched products

--- src/ecommerce-fnt/src/core/Search.js
+++ src/ecommerce-fnt/src/core/Search.js
@@ -82,3 +82,45 @@ const Search = () => {
         <form onSubmit={searchSubmit}>
             <span className="input-group-text">
                 <div className="input-group input-group-lg">
+                    <div className="input-group-prepend">
+                        <select
+                            className="btn mr-2"
+                            onChange={handleChange("category")}
+                        >
+                            <option value="All">All</option>
+                            {categories.map((c, i) => (
+                                <option key={i} value={c._id}>
+                                    {c.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <input
+                        type="search"
+                        className="form-control"
+                        onChange={handleChange("search")}
+                        placeholder="Search by name"
+                    />
+                </div>
+                <div
+                    className="btn input-group-append"
+                    style={{ border: "none" }}
+                >
+                    <button className="input-group-text">Search</button>
+                </div>
+            </span>
+        </form>
+    );
+
+    return (
+        <div className="row">
+            <div className="container mb-3">{searchForm()}</div>
+            <div className="container-fluid mb-3">
+                {searchedProducts(results)}
+            </div>
+        </div>
+    );
+};
+
+export default Search;
