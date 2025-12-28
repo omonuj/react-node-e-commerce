48e60a7c18bcf2189e244b296dd62ca94a58507a
omonuj <jonahodoh@gmail.com>
2025-12-28 19:58:46 +0100

    Add Shop layout with category and price filters

---
+++ src/ecommerce-fnt/src/core/Shop.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import Card from "./Card";
+import { getCategories, getFilteredProducts } from "./apiCore";
+import Checkbox from "./Checkbox";
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPrices";
+
+const Shop = () => {
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
+    });
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(false);
+    const [limit, setLimit] = useState(6);
+    const [skip, setSkip] = useState(0);
+    const [size, setSize] = useState(0);
+    const [filteredResults, setFilteredResults] = useState([]);
+
+    const init = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setCategories(data);
+            }
+        });
+    };
+
+    const loadFilteredResults = newFilters => {
+        // console.log(newFilters);
+        getFilteredProducts(skip, limit, newFilters).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setFilteredResults(data.data);
+                setSize(data.size);
+                setSkip(0);
+            }
+        });
+    };
+
+    const loadMore = () => {
+        let toSkip = skip + limit;
+        // console.log(newFilters);
+        getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
+            if (data.error) {
+                setError(data.error);
