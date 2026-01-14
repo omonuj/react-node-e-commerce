3b03376e2e5b43291399acf8316b54bd9333ae5a
omonuj <jonahodoh@gmail.com>
2026-01-14 10:40:30 +0100

    Handle purchase transaction and order create

--- src/ecommerce-fnt/src/core/Checkout.js
+++ src/ecommerce-fnt/src/core/Checkout.js
@@ -117,3 +117,63 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
             .catch(error => {
                 // console.log("dropin error: ", error);
                 setData({ ...data, error: error.message });
+            });
+    };
+
+    const showDropIn = () => (
+        <div onBlur={() => setData({ ...data, error: '' })}>
+            {data.clientToken !== null && products.length > 0 ? (
+                <div>
+                    <div className="gorm-group mb-3">
+                        <label className="text-muted">Delivery address:</label>
+                        <textarea
+                            onChange={handleAddress}
+                            className="form-control"
+                            value={data.address}
+                            placeholder="Type your delivery address here..."
+                        />
+                    </div>
+
+                    <DropIn
+                        options={{
+                            authorization: data.clientToken,
+                            paypal: {
+                                flow: 'vault'
+                            }
+                        }}
+                        onInstance={instance => (data.instance = instance)}
+                    />
+                    <button onClick={buy} className="btn btn-success btn-block">
+                        Pay
+                    </button>
+                </div>
+            ) : null}
+        </div>
+    );
+
+    const showError = error => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = success => (
+        <div className="alert alert-info" style={{ display: success ? '' : 'none' }}>
+            Thanks! Your payment was successful!
+        </div>
+    );
+
+    const showLoading = loading => loading && <h2 className="text-danger">Loading...</h2>;
+
+    return (
+        <div>
+            <h2>Total: ${getTotal()}</h2>
+            {showLoading(data.loading)}
+            {showSuccess(data.success)}
+            {showError(data.error)}
+            {showCheckout()}
+        </div>
+    );
+};
+
+export default Checkout;
