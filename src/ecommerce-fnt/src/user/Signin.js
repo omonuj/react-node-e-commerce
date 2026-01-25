63ec1136fe6265f4d92c79cf682b0019e3667943
omonuj <jonahodoh@gmail.com>
2026-01-25 18:17:33 +0100

    Redirect by role after auth

--- src/ecommerce-fnt/src/user/Signin.js
+++ src/ecommerce-fnt/src/user/Signin.js
@@ -52,3 +52,58 @@ const Signin = () => {
                 <label className="text-muted">Password</label>
                 <input
                     onChange={handleChange("password")}
+                    type="password"
+                    className="form-control"
+                    value={password}
+                />
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+            </button>
+        </form>
+    );
+
+    const showError = () => (
+        <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+        >
+            {error}
+        </div>
+    );
+
+    const showLoading = () =>
+        loading && (
+            <div className="alert alert-info">
+                <h2>Loading...</h2>
+            </div>
+        );
+
+    const redirectUser = () => {
+        if (redirectToReferrer) {
+            if (user && user.role === 1) {
+                return <Redirect to="/admin/dashboard" />;
+            } else {
+                return <Redirect to="/user/dashboard" />;
+            }
+        }
+        if (isAuthenticated()) {
+            return <Redirect to="/" />;
+        }
+    };
+
+    return (
+        <Layout
+            title="Signin"
+            description="Signin to Node React E-commerce App"
+            className="container col-md-8 offset-md-2"
+        >
+            {showLoading()}
+            {showError()}
+            {signUpForm()}
+            {redirectUser()}
+        </Layout>
+    );
+};
+
+export default Signin;
