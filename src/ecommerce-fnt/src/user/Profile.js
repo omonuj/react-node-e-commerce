84ec564cef90cc1d6cfe2654f6b3996f2fc74485
omonuj <jonahodoh@gmail.com>
2026-01-27 20:47:37 +0100

    Load Profile by user id

---
+++ src/ecommerce-fnt/src/user/Profile.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../core/Layout';
+import { isAuthenticated } from '../auth';
+import { Link, Redirect } from 'react-router-dom';
+import { read, update, updateUser } from './apiUser';
+
+const Profile = ({ match }) => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: false,
+        success: false
+    });
+
+    const { token } = isAuthenticated();
+    const { name, email, password, error, success } = values;
+
+    const init = userId => {
+        // console.log(userId);
+        read(userId, token).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: true });
+            } else {
+                setValues({ ...values, name: data.name, email: data.email });
+            }
+        });
+    };
+
+    useEffect(() => {
+        init(match.params.userId);
+    }, []);
+
+    const handleChange = name => e => {
+        setValues({ ...values, error: false, [name]: e.target.value });
+    };
+
+    const clickSubmit = e => {
+        e.preventDefault();
+        update(match.params.userId, token, { name, email, password }).then(data => {
+            if (data.error) {
+                // console.log(data.error);
+                alert(data.error);
+            } else {
+                updateUser(data, () => {
+                    setValues({
