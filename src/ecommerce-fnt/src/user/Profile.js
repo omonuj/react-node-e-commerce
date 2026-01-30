e6a2f8da788bdd912f780304217080523537cb54
omonuj <jonahodoh@gmail.com>
2026-01-30 17:03:28 +0100

    Submit profile update

--- src/ecommerce-fnt/src/user/Profile.js
+++ src/ecommerce-fnt/src/user/Profile.js
@@ -44,3 +44,50 @@ const Profile = ({ match }) => {
             } else {
                 updateUser(data, () => {
                     setValues({
+                        ...values,
+                        name: data.name,
+                        email: data.email,
+                        success: true
+                    });
+                });
+            }
+        });
+    };
+
+    const redirectUser = success => {
+        if (success) {
+            return <Redirect to="/cart" />;
+        }
+    };
+
+    const profileUpdate = (name, email, password) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="text" onChange={handleChange('name')} className="form-control" value={name} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input type="email" onChange={handleChange('email')} className="form-control" value={email} />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input type="password" onChange={handleChange('password')} className="form-control" value={password} />
+            </div>
+
+            <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+            </button>
+        </form>
+    );
+
+    return (
+        <Layout title="Profile" description="Update your profile" className="container-fluid">
+            <h2 className="mb-4">Profile update</h2>
+            {profileUpdate(name, email, password)}
+            {redirectUser(success)}
+        </Layout>
+    );
+};
+
+export default Profile;
