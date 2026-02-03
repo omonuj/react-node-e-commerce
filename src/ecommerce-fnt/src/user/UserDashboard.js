6c1ecbccf9da1082baa358e425f68e758a2907bd
omonuj <jonahodoh@gmail.com>
2026-02-03 22:18:39 +0100

    Render purchase history

--- src/ecommerce-fnt/src/user/UserDashboard.js
+++ src/ecommerce-fnt/src/user/UserDashboard.js
@@ -37,3 +37,42 @@ const Dashboard = () => {
                             My Cart
                         </Link>
                     </li>
+                    <li className="list-group-item">
+                        <Link className="nav-link" to={`/profile/${_id}`}>
+                            Update Profile
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    };
+
+    const userInfo = () => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">User Information</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">{name}</li>
+                    <li className="list-group-item">{email}</li>
+                    <li className="list-group-item">
+                        {role === 1 ? "Admin" : "Registered User"}
+                    </li>
+                </ul>
+            </div>
+        );
+    };
+
+    const purchaseHistory = history => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">Purchase history</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        {history.map((h, i) => {
+                            return (
+                                <div>
+                                    <hr />
+                                    {h.products.map((p, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <h6>Product name: {p.name}</h6>
