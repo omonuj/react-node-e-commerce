5c31a9c1e0856d0e1e503ea9d9a136b05e3a636a
omonuj <jonahodoh@gmail.com>
2026-02-13 22:07:45 +0100

    Add category admin calls

--- src/ecommerce-fnt/src/admin/apiAdmin.js
+++ src/ecommerce-fnt/src/admin/apiAdmin.js
@@ -56,3 +56,61 @@ export const getCategory = categoryId => {
     return fetch(`${API}/category/${categoryId}`, {
         method: 'GET'
     })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const listOrders = (userId, token) => {
+    return fetch(`${API}/order/list/${userId}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getStatusValues = (userId, token) => {
+    return fetch(`${API}/order/status-values/${userId}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+    return fetch(`${API}/order/${orderId}/status/${userId}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ status, orderId })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
