f86f5f975c5c9cdd17704fb728b8dc64a69598c9
omonuj <jonahodoh@gmail.com>
2026-02-20 20:09:07 +0100

    Submit create category

--- src/ecommerce-fnt/src/admin/AddCategory.js
+++ src/ecommerce-fnt/src/admin/AddCategory.js
@@ -42,3 +42,48 @@ const AddCategory = () => {
                     onChange={handleChange}
                     value={name}
                     autoFocus
+                    required
+                />
+            </div>
+            <button className="btn btn-outline-primary">Create Category</button>
+        </form>
+    );
+
+    const showSuccess = () => {
+        if (success) {
+            return <h3 className="text-success">{name} is created</h3>;
+        }
+    };
+
+    const showError = () => {
+        if (error) {
+            return <h3 className="text-danger">Category should be unique</h3>;
+        }
+    };
+
+    const goBack = () => (
+        <div className="mt-5">
+            <Link to="/admin/dashboard" className="text-warning">
+                Back to Dashboard
+            </Link>
+        </div>
+    );
+
+    return (
+        <Layout
+            title="Add a new category"
+            description={`G'day ${user.name}, ready to add a new category?`}
+        >
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    {showSuccess()}
+                    {showError()}
+                    {newCategoryFom()}
+                    {goBack()}
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default AddCategory;
