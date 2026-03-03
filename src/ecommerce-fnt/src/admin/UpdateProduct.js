5d42ece3b31b075888178781c7a82579a8462a06
omonuj <jonahodoh@gmail.com>
2026-03-03 23:55:37 +0100

    Prefill update product form

---
+++ src/ecommerce-fnt/src/admin/UpdateProduct.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../core/Layout';
+import { isAuthenticated } from '../auth';
+import { Link, Redirect } from 'react-router-dom';
+import { getProduct, getCategories, updateProduct } from './apiAdmin';
+
+const UpdateProduct = ({ match }) => {
+    const [values, setValues] = useState({
+        name: '',
+        description: '',
+        price: '',
+        categories: [],
+        category: '',
+        shipping: '',
+        quantity: '',
+        photo: '',
+        loading: false,
+        error: false,
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    });
+    const [categories, setCategories] = useState([]);
+
+    const { user, token } = isAuthenticated();
+    const {
+        name,
+        description,
+        price,
+        // categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values;
+
+    const init = productId => {
+        getProduct(productId).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                // populate the state
+                setValues({
+                    ...values,
+                    name: data.name,
+                    description: data.description,
+                    price: data.price,
+                    category: data.category._id,
+                    shipping: data.shipping,
+                    quantity: data.quantity,
+                    formData: new FormData()
+                });
+                // load categories
+                initCategories();
+            }
+        });
+    };
+
+    // load categories and set form data
+    const initCategories = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
