6018527f8d1edc246d20af25f04781cfaf246c14
omonuj <jonahodoh@gmail.com>
2026-03-11 08:22:08 +0100

    Submit category update

--- src/ecommerce-fnt/src/admin/updateCategory.js
+++ src/ecommerce-fnt/src/admin/updateCategory.js
@@ -65,3 +65,70 @@ const UpdateCategory = ({ match }) => {
             <form className="mb-5" onSubmit={submitCategoryForm}>
                 <span className="login100-form-title p-b-32 m-b-7">Update Category Form</span>
                 <span className="txt1 p-b-11">Category Name</span>
+                <br />
+                <br />
+                <div className="wrap-input100 validate-input m-b-36">
+                    <input
+                        onChange={handleChange('name')}
+                        value={name}
+                        className="input100"
+                        type="text"
+                        required
+                        name="name"
+                    />
+                </div>
+                <div className="w-size25">
+                    <button type="submit" className="flex-c-m size2 bg1 bo-rad-23 hov1 m-text3 trans-0-4">
+                        Save Changes
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+
+    const showError = () => (
+        <div className={'alert alert-danger'} role="alert" style={{ display: error ? '' : 'none' }}>
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            {error}
+        </div>
+    );
+
+    const redirectUser = () => {
+        if (redirectToProfile) {
+            if (!error) {
+                return <Redirect to="/admin/categories" />;
+            }
+        }
+    };
+
+    const goBackBTN = () => {
+        return (
+            <div className="mt-5">
+                <Link to="/admin/categories" className="text-info">
+                    Back To Admin Home
+                </Link>
+            </div>
+        );
+    };
+
+    return (
+        <Layout
+            title={`Hi ${user.name}`}
+            description={`This is Update Product Action Page`}
+            className="container-fluid"
+        >
+            <div className="row">
+                <div className="col-md-8 offset-md-2 m-b-250 mb-5">
+                    {showError()}
+                    {updateCategoryForm()}
+                    {goBackBTN()}
+                    {redirectUser()}
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default UpdateCategory;
