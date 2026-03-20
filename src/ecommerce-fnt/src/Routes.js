7ca5ceb50de1831235c0909fae5791577adc76c1
omonuj <jonahodoh@gmail.com>
2026-03-20 12:39:44 +0100

    Add React Router route table

---
+++ src/ecommerce-fnt/src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Signup from './user/Signup';
+import Signin from './user/Signin';
+import Home from './core/Home';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './user/UserDashboard';
+import AdminRoute from './auth/AdminRoute';
+import AdminDashboard from './user/AdminDashboard';
+import AddCategory from './admin/AddCategory';
+import AddProduct from './admin/AddProduct';
+import Shop from './core/Shop';
+import Product from './core/Product';
+import Cart from './core/Cart';
+import Orders from './admin/Orders';
+import Profile from './user/Profile';
+import ManageProducts from './admin/ManageProducts';
+import UpdateProduct from './admin/UpdateProduct';
+import UpdateCategory from './admin/updateCategory';
+
+const Routes = () => {
+    return (
